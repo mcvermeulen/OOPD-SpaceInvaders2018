e36d17b44a314676f3f161177960386b96b8a753
Marcel Vermeulen <vermeulen.mc@gmail.com>
2018-04-28 11:59:46 +0200

    initial

---
+++ src/main/java/nl/han/ica/SpaceInvaders2018/SpaceInvaders.java
@@ -0,0 +1,31 @@
+package nl.han.ica.SpaceInvaders2018;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Engine.GameEngine;
+import nl.han.ica.OOPDProcessingEngineHAN.View.View;
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class SpaceInvaders extends GameEngine {
+    public static void main(String[] args) {
+        PApplet.main(new String[]{"nl.han.ica.SpaceInvaders2018.SpaceInvaders"});
+    }
+
+    @Override
+    public void setupGame() {
+        createView(1280, 800);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    private void createView(int viewWidth, int viewHeight) {
+        View view = new View(viewWidth, viewHeight);
+        PImage backgroundImg = loadImage("nl/han/ica/SpaceInvaders2018/media/background-1280x800-2.jpg");
+        view.setBackground(backgroundImg);
+
+        size(viewWidth, viewHeight);
+        setView(view);
+    }
+}
