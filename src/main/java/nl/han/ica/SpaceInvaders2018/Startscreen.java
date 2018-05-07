91cfc055ffb8d2eff909081a77f491cfd2a92697
Marcel Vermeulen <vermeulen.mc@gmail.com>
2018-05-07 11:52:55 +0200

    startscherm

--- src/main/java/nl/han/ica/SpaceInvaders2018/Startscreen.java
+++ src/main/java/nl/han/ica/SpaceInvaders2018/Startscreen.java
@@ -1,17 +1,18 @@
 package nl.han.ica.SpaceInvaders2018;
 
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.GameObject;
-import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.TextObject;
 import processing.core.PGraphics;
+import processing.core.PImage;
 
 public class Startscreen extends GameObject{
-    Sprite logo;
-    SpaceInvaders world;
-    TextObject scoreTabel, player1, play;
+    private PImage logo;
+    private SpaceInvaders world;
+    private TextObject scoreTabel, player1, play;
 
     public Startscreen(SpaceInvaders world) {
-        this.logo = new Sprite("nl/han/ica/SpaceInvaders2018/media/titel.png");
+        this.logo = world.loadImage("nl/han/ica/SpaceInvaders2018/media/titel.png");
+        this.world = world;
         this.width = world.getPlayfieldWidth();
         this.height = world.getPlayfieldHeight();
         this.x = (world.getWidth() - width)/2;;
@@ -21,15 +22,25 @@ public class Startscreen extends GameObject{
     @Override
     public void mouseClicked(int x, int y, int button) {
         System.out.println("klik");
+        if (world.getGameState() == 0) {
+            world.newGame();
+        }
+
     }
 
     @Override
     public void update() {
-
+        if (world.getGameState() != 0) {
+            this.setVisible(false);
+        } else {
+            this.setVisible(true);
+        }
     }
 
     @Override
     public void draw(PGraphics g) {
+        g.imageMode(CENTER);
+        g.image(logo, x+width/2, y+logo.height);
     }
 
 }
